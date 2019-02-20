82ce5fc3b5f005b633084b8d32cfd1eebb6b6d2e
Torben Brodt <tb@plista.com>
2019-02-20 09:53:51 +0100

    Write to postgres

--- app/dbwrite.py
+++ app/dbwrite.py
@@ -1,12 +1,17 @@
 from app.entry import Entry
 import psycopg2
+import os
 
 
 def process_row_to_db(row: Entry):
-    conn = psycopg2.connect("dbname='template1' user='dbuser' host='localhost' password='dbpass'")
+    conn = psycopg2.connect(dbname=os.getenv("DB_NAME"), user=os.getenv("DB_USER"), host=os.getenv("DB_HOST"), password=os.getenv("DB_PASSWORD"))
 
-    insert_stmt = "INSERT OR IGNORE INTO adstxt " \
-                  "(SITE_DOMAIN, EXCHANGE_DOMAIN, SELLER_ACCOUNT_ID, ACCOUNT_TYPE, TAG_ID, ENTRY_COMMENT) " \
-                  "VALUES (?, ?, ?, ?, ?, ? );"
+    insert_stmt = "INSERT INTO adstxt " \
+                  "(SITE_DOMAIN, EXCHANGE_DOMAIN, SELLER_ACCOUNT_ID, ACCOUNT_TYPE, TAG_ID) " \
+                  "VALUES (%s, %s, %s, %s, %s);"
+    args = (row.site_domain, row.exchange_domain, row.seller_account_id, row.account_type, row.tag_id)
 
-    print("%s %s %s %s %s" % (row.site_domain, row.exchange_domain, row.seller_account_id, row.account_type, row.tag_id))
+    cur = conn.cursor()
+    cur.execute(insert_stmt, args)
+
+    conn.commit()
