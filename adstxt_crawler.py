c608c3f3ba1d36c9d9e6c217dd4c653ce31b65ac
Torben Brodt <tb@plista.com>
2019-11-04 12:21:27 +0100

    Fix wrong target paramter

--- adstxt_crawler.py
+++ adstxt_crawler.py
@@ -6,7 +6,7 @@ import sys
 parser = argparse.ArgumentParser(description='Loads adstxt from publishers and matches with filter.')
 parser.add_argument('--url', help='Source file with 1 line per publisher')
 parser.add_argument('--source', help='Source file with 1 line per publisher')
-parser.add_argument('--target', help='Target file with 1 line per publisher')
+parser.add_argument('--target', help='Target file with 1 line per publisher', required=True)
 parser.add_argument('--filter', help='e.g. appnexus.com, 3563, DIRECT', required=True, action='append')
 parser.add_argument('--verbose', help="increase output verbosity", action="store_true")
 args = parser.parse_args()
@@ -19,5 +19,5 @@ if args.url:
     app.queue(args.url)
 if args.source:
     app.queueFile(args.source)
-metrics = app.run(app.target)
+metrics = app.run(args.target)
 print(metrics)
